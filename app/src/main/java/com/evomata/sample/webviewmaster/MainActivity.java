edde394b377e8b3c3fd8d03d31d9247c1ce7000e
Hitesh Suvarna <hitesh@evomata.com>
2020-04-29 18:09:41 +0530

    Added Code in Main Activity to allow navigation in the same window

--- app/src/main/java/com/evomata/sample/webviewmaster/MainActivity.java
+++ app/src/main/java/com/evomata/sample/webviewmaster/MainActivity.java
@@ -3,6 +3,7 @@ package com.evomata.sample.webviewmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView wv1;
     String url_navigation = (String) "http://www.evomata.com";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +21,25 @@ public class MainActivity extends AppCompatActivity {
         wv1=(WebView)findViewById(R.id.webview);
         wv1.setWebViewClient(new MyBrowser());
 
+
         wv1.getSettings().setLoadsImagesAutomatically(true);
         wv1.getSettings().setJavaScriptEnabled(true);
         wv1.loadUrl(url_navigation);
     }
+
+//    TO USE PHONE'S BACK BUTTON TO NAVIGATE TO PREVIOUS WINDOW
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && wv1.canGoBack()) {
+            wv1.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     private class MyBrowser extends WebViewClient {
         @Override
