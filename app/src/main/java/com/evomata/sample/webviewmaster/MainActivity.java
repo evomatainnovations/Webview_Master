15d06b9dd20f1e0771903cb67157c626134a2a81
Hitesh Suvarna <hitesh@evomata.com>
2020-04-29 17:11:49 +0530

    Initial Commit after config git --global user.name & user.email

---
+++ app/src/main/java/com/evomata/sample/webviewmaster/MainActivity.java
@@ -0,0 +1,17 @@
+package com.evomata.sample.webviewmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+    }
+}
