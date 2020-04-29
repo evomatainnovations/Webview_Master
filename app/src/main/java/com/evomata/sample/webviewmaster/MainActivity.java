cf4200d59c0e1825916573f65dea606d004a1361
Hitesh Suvarna <hitesh@evomata.com>
2020-04-29 17:39:14 +0530

    Added Basic Webview Code & Network Security Config File to allow Clear text url or http only URL's

--- app/src/main/java/com/evomata/sample/webviewmaster/MainActivity.java
+++ app/src/main/java/com/evomata/sample/webviewmaster/MainActivity.java
@@ -3,15 +3,32 @@ package com.evomata.sample.webviewmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView wv1;
+    String url_navigation = (String) "http://www.evomata.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView browser = (WebView) findViewById(R.id.webview);
+        wv1=(WebView)findViewById(R.id.webview);
+        wv1.setWebViewClient(new MyBrowser());
+
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.loadUrl(url_navigation);
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
